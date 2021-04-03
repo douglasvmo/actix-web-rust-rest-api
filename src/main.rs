86e9a7d55739ce46476720ada7bb1a5c4374baa2
Douglas Oliveira <douglas4030@gmail.com>
2021-04-03 20:52:09 -0300

    feat: crud of books on diesel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::{Connection, PgConnection};
+use dotenv::dotenv;
+use std::env;
+
+pub mod models;
+pub mod schema;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    let conn = PgConnection::establish(&database_url).unwrap();
+
+    let book = models::NewBooks {
+        title: String::from("The Little Prince"),
+        author: String::from("Antoine de Saint-Exupéry"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn) {
+        println!("success")
+    } else {
+        println!("failed")
+    }
 }
---
+++ src/models.rs
@@ -0,0 +1,70 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::PgConnection;
+
+use crate::schema::books;
+use crate::schema::books::dsl::books as all_books;
+
+#[derive(Queryable)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name = "books"]
+pub struct NewBooks {
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
+
+impl Book {
+    pub fn show(id: i32, conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .find(id)
+            .load::<Book>(conn)
+            .expect("error loanding book")
+    }
+    pub fn all(conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .order(books::id.desc())
+            .load::<Book>(conn)
+            .expect("error loanding  the books")
+    }
+    pub fn update_by_id(id: i32, conn: &PgConnection, book: NewBooks) -> bool {
+        use crate::schema::books::dsl::{author as a, published as p, title as t};
+
+        let NewBooks {
+            title,
+            author,
+            published,
+        } = book;
+
+        diesel::update(all_books.find(id))
+            .set((a.eq(author), p.eq(published), t.eq(title)))
+            .get_result::<Book>(conn)
+            .is_ok()
+    }
+
+    pub fn insert(book: NewBooks, conn: &PgConnection) -> bool {
+        diesel::insert_into(books::table)
+            .values(&book)
+            .execute(conn)
+            .is_ok()
+    }
+    pub fn delete_by_id(id: i32, conn: &PgConnection) -> bool {
+        if Book::show(id, conn).is_empty() {
+            return false;
+        };
+        diesel::delete(all_books.find(id)).execute(conn).is_ok()
+    }
+    pub fn all_by_author(author: String, conn: &PgConnection) -> Vec<Book> {
+        all_books
+            .filter(books::author.eq(author))
+            .load::<Book>(conn)
+            .expect("error loand books by author")
+    }
+}
